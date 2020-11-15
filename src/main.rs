11065ed0f4fc3e2235ffe8c49838ddc106e5d28f
Rowland Rose <rowlandrose@gmail.com>
2020-11-14 20:40:28 -0500

    Heightmap tweaks finished to point of mountains

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ use std::time::Instant; // for timer
 
 const TILE_SIZE: u8 = 16;
 const HEIGHTMAP_RANGE: u8 = 100;
+const CUTOFF_TERRAIN: u8 = 80;
+const CUTOFF_WATER: u8 = 50;
+const RIVER_START_MIN_DIST: u8 = 5;
 
 struct Tile {
     name: String,
@@ -48,13 +51,39 @@ fn normalize_heightmap_to_range(
     max_exclusive: u32
 ) {
 
-    let r = heightmap.range();
+    // This section if you want to try Heightmap's built-in range function again
+    /*let r = heightmap.range();
     let new_max = r.1 - r.0;
+    println!("Heightmap range: min {}, max {}.", r.0, r.1);*/
+
+    // Using range found manually, since range function built into Heightmap seems incorrect
+    let mut max = 0.0;
+    for x in 0..cells {
+        for y in 0..cells {
+            let old_val = heightmap.get(x, y);
+            if old_val > max {
+                max = old_val;
+            }
+        }
+    }
+    let mut min = max;
+    for x in 0..cells {
+        for y in 0..cells {
+            let old_val = heightmap.get(x, y);
+            if old_val < min {
+                min = old_val;
+            }
+        }
+    }
+
+    let new_max = max - min;
+
+    println!("Manual range: min {}, max {}.", min, max);
 
     for x in 0..cells {
         for y in 0..cells {
             let old_val = heightmap.get(x, y);
-            heightmap.set(x, y, (old_val - r.0) * ((max_exclusive as f32)-1.0) / new_max);
+            heightmap.set(x, y, ((old_val - min) / new_max) * (max_exclusive as f32 - 1.0));
         }
     }
 }
@@ -70,6 +99,14 @@ fn test_png(heightmap: &mut Heightmap<f32>, cells: u32, filename: &str) {
         Err(e) => println!("Directory \"rendered_images\" already exists.")
     };
     img.save(["rendered_images/", filename, ".png"].concat()).unwrap();
+}
+
+fn distance(x1: i32, y1: i32, x2: i32, y2: i32) -> f32 {
+
+    let a:i32 = x1 - x2;
+    let b:i32 = y1 - y2;
+
+    ((a*a + b*b) as f32).sqrt() as f32
 }
 
 fn main() {
@@ -149,7 +186,6 @@ fn main() {
     // Reset heightmap to desired range
     normalize_heightmap_to_range(&mut heightmap, cells, HEIGHTMAP_RANGE as u32);
 
-    // Write test png to see heightmap at this stage
     test_png(&mut heightmap, cells, "test");
 
     // Blend heightmap with a simplex noise heightmap
@@ -176,13 +212,88 @@ fn main() {
         }
     }
 
-    // Write test png to see heightmap at this stage
     test_png(&mut heightmap, cells, "test2");
 
     // print one row of cell values for test
     /*for cell in 0..cells {
         println!("Heightmap value: {}", heightmap.get(cell, 0));
     }*/
+
+    // Gradually make edges of map ocean
+    let center_x = (cells / 2) - 1;
+    let center_y = (cells / 2) - 1;
+
+    let land_radius = cells as f32 * 0.32;
+
+    for x in 0..cells {
+        for y in 0..cells {
+
+            let dist = distance(center_x as i32, center_y as i32, x as i32, y as i32);
+
+            if dist > land_radius {
+
+                let further = dist - land_radius;
+
+                let old_val = heightmap.get(x, y);
+                let mut new_val = old_val * ((land_radius - (further)) / land_radius);
+                if new_val < 0.0 || x == 0 || y == 0 || x == cells - 1 || y == cells - 1 {
+                    new_val = 0.0;
+                }
+
+                heightmap.set(x, y, new_val);
+            }
+        }
+    }
+
+    test_png(&mut heightmap, cells, "test3");
+
+    // Reset heightmap to desired range
+    normalize_heightmap_to_range(&mut heightmap, cells, HEIGHTMAP_RANGE as u32);
+
+    test_png(&mut heightmap, cells, "test4");
+
+    // Get another diamond-square heightmap (with no island) and combine with 
+    // original where there is land. Will result in more varied mountains, 
+    // instead of all being in the center of the landmass.
+    let mut heightmap_m = Heightmap::new_flat((cells, cells), (0.0, 0.0));
+    diamond_square(&mut heightmap_m, 0, &mut rng, distr).unwrap();
+
+    // Reset heightmap to desired range
+    normalize_heightmap_to_range(&mut heightmap_m, cells, HEIGHTMAP_RANGE as u32);
+
+    // Combining new mountain heightmap with original heightmap
+    for x in 0..cells {
+        for y in 0..cells {
+
+            let orig_val = heightmap.get(x, y);
+            let mountain_val = heightmap_m.get(x, y);
+
+            let mut new_val = orig_val;
+
+            if orig_val >= CUTOFF_WATER as f32 {
+
+                let diff = mountain_val - orig_val;
+
+                let adjust;
+
+                if orig_val >= CUTOFF_TERRAIN as f32 {
+                    adjust = diff / 10.0;
+                } else {
+                    adjust = diff;
+                }
+
+                new_val = orig_val + adjust;
+
+                if new_val < CUTOFF_WATER as f32 {
+                    new_val = CUTOFF_WATER as f32;
+                }
+            }
+
+            heightmap.set(x, y, new_val);
+        }
+    }
+
+    test_png(&mut heightmap, cells, "test5");
 
     println!("Script finished in {} seconds.", now.elapsed().as_secs_f32());
 }
